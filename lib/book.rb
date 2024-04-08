72ac2a9b102fca4a48c666375e0ca3fd692c7498
vinnie <githinjigicheru146@gmail.com>
2024-04-08 09:46:52 +0300

    Cobble shoe and publish book

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
-
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+    def initialize(title="And Then There Were None")
+        @title = title
+    end
+    def properties(author , page_count, genre)
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
